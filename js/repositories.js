2b197c4519844d999e720d177f5aa7404c271b71
Ben Kiefer <kieferfam@msn.com>
2014-01-18 13:01:14 -0600

    Ben: switch to list item view with more information about repo

--- js/repositories.js
+++ js/repositories.js
@@ -1,46 +1,47 @@
-    var RepositoryItemView = Backbone.View.extend({
-        tagName: 'tr',
-        className: 'repository-row',
-        template: $('#rowTemplate').html(),
+var RepositoryItemView = Backbone.View.extend({
+    tagName: 'a',
+    className: 'list-group-item',
+    attributes: {"href": "#"},
+    template: $('#rowTemplate').html(),
 
-        render: function () {
-            var tmpl = _.template(this.template);
-            this.$el.html(tmpl(this.model.toJSON()));
-            return this;
-        }
-    });
+    render: function () {
+        var tmpl = _.template(this.template);
+        this.$el.html(tmpl(this.model.toJSON()));
+        return this;
+    }
+});
 
-    var RepositoryListView = Backbone.View.extend({
-        el: $("table#repositories tbody"),
+var RepositoryListView = Backbone.View.extend({
+    el: $("div#repositories"),
 
-        initialize: function () {
-            this.collection = new RepositoryList();
-            this.listenTo(this.collection, 'reset add change remove', this.render, this);
-            this.collection.fetch();
-        },
+    initialize: function () {
+        this.collection = new RepositoryList();
+        this.listenTo(this.collection, 'reset add change remove', this.render, this);
+        this.collection.fetch();
+    },
 
-        render: function () {
-            var that = this;
-            _.each(this.collection.models, function (item) {
-                that.renderRepo(item);
-            }, this);
-        },
+    render: function () {
+        var that = this;
+        _.each(this.collection.models, function (item) {
+            that.renderRepo(item);
+        }, this);
+    },
 
-        renderRepo: function (item) {
-            var repoView = new RepositoryItemView({
-                model: item
-            });
-            var val = repoView.render().el;
-            this.$el.append(val);
-        }
-    });
+    renderRepo: function (item) {
+        var repoView = new RepositoryItemView({
+            model: item
+        });
+        this.$el.append(repoView.render().el);
+    }
+});
 
-    var Repository = Backbone.Model.extend({ });
+var Repository = Backbone.Model.extend({ });
 
-    var RepositoryList = Backbone.Collection.extend({
-        model: Repository,
-        url: 'https://api.github.com/users/benkiefer/repos',
-        parse: function (response) {
-            return response;
-        }
-    });
+var RepositoryList = Backbone.Collection.extend({
+    model: Repository,
+    url: 'https://api.github.com/users/benkiefer/repos',
+    parse: function (response) {
+        console.log(response[0]);
+        return response;
+    }
+});
